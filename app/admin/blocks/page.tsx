61d602f0579c78459dd48bd3ee9723a7ee085185
proyect2024-admin <abraham18eda@gmail.com>
2026-03-07 02:00:33 -0600

    corrige ruta cms

--- app/admin/blocks/page.tsx
+++ app/admin/blocks/page.tsx
@@ -1,6 +1,19 @@
 export default async function BlocksPage() {
-  const blocks = await fetch('http://localhost:3000/api/cms/blocks')
-    .then(res => res.json());
+  // Usamos la variable de entorno para obtener la URL de la API
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  // Verificamos si la URL está definida, si no, lanzamos un error
+  if (!apiUrl) {
+    throw new Error('API URL is not defined.');
+  }
+  
+   // Realizamos la solicitud usando la URL correcta
+  const blocks = await fetch(`${apiUrl}/api/cms/blocks`)
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error('Error fetching blocks:', error);
+      return [];
+    });
 
   return (
     <div>
