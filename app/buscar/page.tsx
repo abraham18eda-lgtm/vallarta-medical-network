f86e79e63e52a85a3380c6db8539ad96abdfe021
proyect2024-admin <abraham18eda@gmail.com>
2026-04-27 03:30:06 -0600

    se agrenan paginas de admin, y modelos

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -1,27 +1,10 @@
-"use client"
-import { useEffect, useState } from "react"
-import { useSearchParams } from "next/navigation"
-import DoctorCard from "@/components/utils/DoctorCard"
+import { Suspense } from "react"
+import BuscarPage from "@/components/home/BuscarPage"
 
-export default function BuscarPage() {
-  const params = useSearchParams()
-
-  const [doctors, setDoctors] = useState([])
-
-  const query = params.get("query") || ""
-  const city = params.get("city") || ""
-
-  useEffect(() => {
-    fetch(`/api/doctors?query=${query}&city=${city}`)
-      .then(res => res.json())
-      .then(setDoctors)
-  }, [query, city])
-
+export default function Page() {
   return (
-    <div className="p-6 grid md:grid-cols-3 gap-6">
-      {doctors.map((doc: any) => (
-        <DoctorCard key={doc.id} doctor={doc} />
-      ))}
-    </div>
+    <Suspense fallback={<div>Cargando...</div>}>
+      <BuscarPage />
+    </Suspense>
   )
 }
