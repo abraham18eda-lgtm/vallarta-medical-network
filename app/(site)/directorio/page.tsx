6bd3154e8f65ce173f1c31d8762335df0025f3ca
proyect2024-admin <abraham18eda@gmail.com>
2026-07-07 02:46:20 -0600

    fix Router

--- app/(site)/directorio/page.tsx
+++ app/(site)/directorio/page.tsx
@@ -1,19 +1,41 @@
 import DoctorsList from "@/components/home/DoctorsList"
 
-export default function Page({ searchParams }: any) {
-  const page = searchParams.page || 1
-  const search = searchParams.search || ""
-  const city = searchParams.city || ""
+type Props = {
+  params: Promise<{
+    locale: "es" | "en"
+  }>
+  searchParams: Promise<{
+    page?: string
+    search?: string
+    city?: string
+  }>
+}
+
+export default async function Page({
+  params,
+  searchParams
+}: Props) {
+
+  const { locale } = await params
+  const query = await searchParams
+
+  const page = query.page || "1"
+  const search = query.search || ""
+  const city = query.city || ""
 
   return (
     <div className="bg-gray-50 min-h-screen p-6">
 
       <div className="max-w-7xl mx-auto">
+
         <h1 className="text-2xl font-bold mb-6">
-          Directorio de Doctores
+          {locale === "en"
+            ? "Doctors Directory"
+            : "Directorio de Doctores"}
         </h1>
 
-        <DoctorsList  />
+        <DoctorsList locale={locale} />
+
       </div>
 
     </div>
